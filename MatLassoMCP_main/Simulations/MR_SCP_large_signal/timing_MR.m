ce5dfeecbe4a559d5f7e604cb1e91781565cd865
LeiShi-rocks <leishi1998@gmail.com>
2024-01-13 22:17:08 -0800

    update timing computation for the MR model

---
+++ MatLassoMCP_main/Simulations/MR_SCP_large_signal/timing_MR.m
@@ -0,0 +1,363 @@
+%% varying N
+clear;clc;
+fprintf('Running: MR_SCP_LARGE_SIGNAL, varying N... \n\n')
+
+nr = 50;
+nc = 50;
+
+N_Cand = [5e2, 1e3, 2e3];
+r = 5;
+noise = struct(...
+    'type', 'Gaussian', ...
+    'scale', 1.0, ...
+    'para', 0.1);
+problem = 'MR';
+cp_opts = struct(...
+    'num_seg', 2,...
+    'pos_seg', [0, 0.5]);
+design = struct(...
+    'type', 'AR',...
+    'para', 0);
+
+running_flag = [1,1,1,1];
+
+num_iter = 25;
+record_N = zeros(4, 3, num_iter);
+% 4 methods: ours, LASSO, Oracle, NC
+% Fnormsq_1, Fnormsq_2, tau_hat, obj_path, Delta_path, Theta_hat,
+% Theta_star(in first round of running)
+
+save_flag = 1;
+
+
+rng(2022);
+
+for N_ind = 1:length(N_Cand)
+    N = N_Cand(N_ind);
+    for iter = 1:num_iter
+        % data generation
+        [y, X, outInfo] = DataGenMCP(nr, nc, N, r, noise, problem, design, cp_opts);
+        Theta_star = outInfo.Theta_star;
+        threshold_var = outInfo.threshold_var;
+        
+        % solve MatLassoSCP
+        if running_flag(1)
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 5e3);
+            APG_opts = struct(...
+                'type', type,...
+                'Clambda', 0.15,...
+                'tol', 1e-4,...
+                'maxiter', 1e2,...
+                'Theta_init', zeros(nr, 2*nc));
+            
+            tic;
+            [Theta_Delta_hat, tau_hat, obj_path, Delta_path] = MatLassoSCP(y, X, threshold_var, 0.2, [0,1], 100, APG_opts);
+            elapsedtime = toc;
+            
+            % store information
+            Fnormsq_1 = sum(sum((Theta_Delta_hat(:,1:nc) - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_Delta_hat(:,1:nc) + Theta_Delta_hat(:,(nc+1):(2*nc)) - Theta_star(:,:,2)).^2));
+            
+            record_N(1, N_ind, iter)  = elapsedtime;
+            
+            % output
+            fprintf(' N_ind: %d, Iteration: %d, MatLassoSCP \n', N_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d, tau_hat: %d \n', Fnormsq_1, Fnormsq_2, tau_hat);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+        
+        % solve LASSO
+        if running_flag(2)
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 5e3);
+            APG_opts = struct(...
+                'type', type,...
+                'Clambda', 0.05,...
+                'tol', 1e-4,...
+                'maxiter', 1e2,...
+                'Theta_init', zeros(nr, 2*nc));
+            
+            tic;
+            [Theta_Delta_hat, tau_hat, obj_path, Delta_path] = LassoSCP(y, X, threshold_var, 0.2, [0,1], 100, APG_opts);
+            elapsedtime = toc;
+            
+            % store information
+            Fnormsq_1 = sum(sum((Theta_Delta_hat(:,1:nc) - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_Delta_hat(:,1:nc) + Theta_Delta_hat(:,(nc+1):(2*nc)) - Theta_star(:,:,2)).^2));
+            
+            record_N(2, N_ind, iter)  = elapsedtime;
+            
+            % output
+            fprintf(' N_ind: %d, Iteration: %d, LassoSCP \n', N_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d, tau_hat: %d \n', Fnormsq_1, Fnormsq_2, tau_hat);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+        
+        % solve Oracle
+        if running_flag(3)
+            X_new = zeros(2*nc, N);
+            for i = 1:N
+                X_new(:,i) = [X(:,i); X(:,i) .* (threshold_var(i) > 0.5)];
+            end
+            
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 5e3);
+            Clambda = 0.15;
+            tol = 1e-4;
+            maxiter = 1e2;
+            Theta_init = zeros(nr, 2*nc);
+            
+            tic;
+            [Theta_Delta_hat, ~] = MVAPG_MCP(y, X_new, type, Clambda, tol, maxiter, Theta_init);
+            elapsedtime = toc;
+            
+            % store information
+            Fnormsq_1 = sum(sum((Theta_Delta_hat(:,1:nc) - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_Delta_hat(:,1:nc) + Theta_Delta_hat(:,(nc+1):(2*nc)) - Theta_star(:,:,2)).^2));
+            
+            record_N(3, N_ind, iter)  = elapsedtime;
+            
+            % output
+            fprintf(' N_ind: %d, Iteration: %d, Oracle \n', N_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d \n', Fnormsq_1, Fnormsq_2);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        
+        fprintf('\n');
+        
+        
+        % solve Non change point
+        if running_flag(4)
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 5e3);
+            Clambda = 0.15;
+            tol = 1e-4;
+            maxiter = 1e2;
+            Theta_init = zeros(nr, nc);
+            
+            %[Theta_hat, rank] = MVAPG(y, X, type, lambda, tol, maxiter, Theta_init);
+            tic;
+            [Theta_hat, ~] = MVAPG_MCP(y, X, type, Clambda, tol, maxiter, Theta_init);
+            elapsedtime = toc;
+            
+            Fnormsq_1 = sum(sum((Theta_hat - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_hat - Theta_star(:,:,2)).^2));
+            
+            record_N(4, N_ind, iter)  = elapsedtime;
+            
+            fprintf(' N_ind: %d, Iteration: %d, Non Change \n', N_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d \n', Fnormsq_1, Fnormsq_2);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+    end
+end
+
+if save_flag
+    save('record_N_MR_SCP_large_signal_timing.mat', 'record_N');
+end
+
+
+%% varying dimension
+clear;clc;
+
+fprintf('Running: MR_SCP_LARGE_SIGNAL, varying dimension... \n\n')
+
+dim_Cand = [25, 50, 75];
+
+N_Cand = 4*5*dim_Cand;
+r = 5;
+noise = struct(...
+    'type', 'Gaussian', ...
+    'scale', 1.0, ...
+    'para', 0.1);
+problem = 'MR';
+cp_opts = struct(...
+    'num_seg', 2,...
+    'pos_seg', [0, 0.5]);
+design = struct(...
+    'type', 'AR',...
+    'para', 0);
+
+running_flag = [1,1,1,1];
+
+num_iter = 1;
+record_dim = zeros(4, 3, num_iter);
+% 4 methods: ours, LASSO, Oracle, NC
+% Fnormsq_1, Fnormsq_2, tau_hat, obj_path, Delta_path
+
+save_flag = 1;
+
+rng(2022);
+
+for dim_ind = 1:length(N_Cand)
+    nr = dim_Cand(dim_ind);
+    nc = nr;
+    N = N_Cand(dim_ind);
+    for iter = 1:num_iter
+        % data generation
+        [y, X, outInfo] = DataGenMCP(nr, nc, N, r, noise, problem, design, cp_opts);
+        Theta_star = outInfo.Theta_star;
+        threshold_var = outInfo.threshold_var;
+        
+        % solve MatLassoSCP
+        if running_flag(1)
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 1e4);
+            APG_opts = struct(...
+                'type', type,...
+                'Clambda', 0.15,...
+                'tol', 1e-4,...
+                'maxiter', 2e2,...
+                'Theta_init', zeros(nr, 2*nc));
+            
+            tic;
+            [Theta_Delta_hat, tau_hat, obj_path, Delta_path] = MatLassoSCP(y, X, threshold_var, 0.2, [0,1], 100, APG_opts);
+            elapsedtime = toc;
+            
+            % store information
+            Fnormsq_1 = sum(sum((Theta_Delta_hat(:,1:nc) - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_Delta_hat(:,1:nc) + Theta_Delta_hat(:,(nc+1):(2*nc)) - Theta_star(:,:,2)).^2));
+            
+            record_dim(1, dim_ind, iter)  = elapsedtime;
+            
+            % output
+            fprintf(' dim_ind: %d, Iteration: %d, MatLassoSCP \n', dim_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d, tau_hat: %d \n', Fnormsq_1, Fnormsq_2, tau_hat);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+        
+        % solve LASSO
+        if running_flag(2)
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 1e4);
+            APG_opts = struct(...
+                'type', type,...
+                'Clambda', 0.05,...
+                'tol', 1e-4,...
+                'maxiter', 2e2,...
+                'Theta_init', zeros(nr, 2*nc));
+            
+            tic;
+            [Theta_Delta_hat, tau_hat, obj_path, Delta_path] = LassoSCP(y, X, threshold_var, 0.2, [0,1], 100, APG_opts);
+            elapsedtime = toc;
+            
+            % store information
+            Fnormsq_1 = sum(sum((Theta_Delta_hat(:,1:nc) - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_Delta_hat(:,1:nc) + Theta_Delta_hat(:,(nc+1):(2*nc)) - Theta_star(:,:,2)).^2));
+            
+            record_dim(2, dim_ind, iter)  = elapsedtime;
+            
+            % output
+            fprintf(' dim_ind: %d, Iteration: %d, LassoSCP \n', dim_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d, tau_hat: %d \n', Fnormsq_1, Fnormsq_2, tau_hat);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+        % solve Oracle
+        X_new = zeros(2*nc, N);
+        if running_flag(3)
+            for i = 1:N
+                X_new(:,i) = [X(:,i); X(:,i) .* (threshold_var(i) > 0.5)];
+            end
+            
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 1e4);
+            Clambda = 0.15;
+            tol = 1e-4;
+            maxiter = 2e2;
+            Theta_init = zeros(nr, 2*nc);
+            
+            tic;
+            [Theta_Delta_hat, ~] = MVAPG_MCP(y, X_new, type, Clambda, tol, maxiter, Theta_init);
+            elapsedtime = toc;
+            
+            % store information
+            Fnormsq_1 = sum(sum((Theta_Delta_hat(:,1:nc) - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_Delta_hat(:,1:nc) + Theta_Delta_hat(:,(nc+1):(2*nc)) - Theta_star(:,:,2)).^2));
+            
+            record_dim(3, dim_ind, iter)  = elapsedtime;
+            
+            
+            % output
+            fprintf(' dim_ind: %d, Iteration: %d, Oracle \n', dim_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d \n', Fnormsq_1, Fnormsq_2);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+        
+        % solve Non change point
+        if running_flag(4)
+            type = struct(...
+                'name', 'L2',...
+                'eta', 0.8,...
+                'Lf', 1e4);
+            Clambda = 0.15;
+            tol = 1e-4;
+            maxiter = 2e2;
+            Theta_init = zeros(nr, nc);
+            
+            %[Theta_hat, rank] = MVAPG(y, X, type, lambda, tol, maxiter, Theta_init);
+            tic;
+            [Theta_hat, ~] = MVAPG_MCP(y, X, type, Clambda, tol, maxiter, Theta_init);
+            elapsedtime = toc;
+            
+            Fnormsq_1 = sum(sum((Theta_hat - Theta_star(:,:,1)).^2));
+            Fnormsq_2 = sum(sum((Theta_hat - Theta_star(:,:,2)).^2));
+            
+            % store information
+            record_dim(4, dim_ind, iter) = elapsedtime;
+            
+            % output
+            fprintf(' dim_ind: %d, Iteration: %d, Non Change \n', dim_ind, iter);
+            fprintf(' Fnormsq_1: %d, Fnormsq_2: %d \n', Fnormsq_1, Fnormsq_2);
+            fprintf(' timing: %d \n', elapsedtime);
+        end
+        fprintf('\n');
+        
+    end
+end
+
+if save_flag
+    save('record_dim_MR_SCP_large_signal_timing.mat', 'record_dim');
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
